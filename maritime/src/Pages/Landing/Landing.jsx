2a13e53208cc5b39d291deae0c2eeddf347ef3d3
Chirag <ckd12ka1@gmail.com>
2024-03-06 23:52:04 +0530

    Landing Page Modifications

--- maritime/src/Pages/Landing/Landing.jsx
+++ maritime/src/Pages/Landing/Landing.jsx
@@ -4,16 +4,21 @@ import boatImage from "../../Assets/cargoship.png";
 import Title from "./Title";
 import Navbar from "../Components/Navbar/Navbar.jsx";
 import Footer from "../Components/Footer/Footer.jsx";
+import videoBg from "../../Assets/101.mp4";
+
 
 function Landing() {
   return (
     <>
       <div className="landing-page">
-        <Navbar />
         <div className="waves-section">
           <div className="boat-section">
             <div className="title-section bg-red-300">
+              <Navbar />
               <Title />
+            </div>
+            <div className="video section">
+              <video src={videoBg} autoPlay loop muted />
             </div>
             <img src={boatImage} className="boat-image" />
           </div>
